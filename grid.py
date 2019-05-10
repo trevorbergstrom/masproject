b3281a4a5f25e807729fd6da2c658d07dae6abe8
Adib Mosharrof <adib.mosharrof@gmail.com>
2019-05-10 00:54:43 -0700

    cosmetic changes--renamed methods, added comments and removed unnecessary spaces in code

--- grid.py
+++ grid.py
@@ -19,7 +19,6 @@ class Cell:
 
     def __repr__(self):
         return "(X: %s, Y:%s) " % (self.x, self.y)
-        #return "X: %s, Y:%s, AnimalDensity:%s, HasBeenVisited:%s" % (self.x, self.y, self.animal_density, self.has_been_visited)
         
     def __hash__(self):
         return hash(str(self))
@@ -27,14 +26,13 @@ class Cell:
     def __eq__(self,other):
         return self.x == other.x and self.y == other.y
     
-class GeneratePath:
+
+class GeneratePaths:
     grid = []
     starting_node = None
     max_route_length = 6
     feasible_paths = []
     grid_size = 5
-
-
 
     def init_grid(self):
         for i in range(self.grid_size):
@@ -48,71 +46,64 @@ class GeneratePath:
         starting_y = random.randint(0,self.grid_size-1)
         #self.starting_node = self.grid[starting_x][starting_y]
         self.starting_node = self.grid[0][0]
-        #print("Starting Node ", self.starting_node)
 
-    def generate_route(self, depth, node, parent_node, path):
-        #if limit is reached and currentnode is starting node
-        #add to feasible_paths and return
+    def generate_routes(self, depth, node, path):
+        #add current node to path and mark it as visited
         node.has_been_visited = True
         path.append(node)
         
+        #if limit is reached and currentnode is starting node
+        #add to feasible_paths and return
         if depth is self.max_route_length:
             if node is self.starting_node:
-              if self.is_path_duplicate(path, self.feasible_paths) is False:
+              if self.does_path_cover_same_cells(path, self.feasible_paths) is False:
                 self.feasible_paths.append(copy.deepcopy(path))
-                #return True
-            #path = []
-            #return False
         else:
-        #foreach adjacent node not visited so far
-        #add node to path and increase limit
-            adjacent_nodes = self.get_adjacent_nodes(node, parent_node, path)
-            for adjacent_node in adjacent_nodes:
-                #if adjacent_node.has_been_visited is False:
-                self.generate_route(depth+1, adjacent_node, node, path )
-                    
+            #get adjacent nodes that are not in the path being generated
+            nodes_to_expand = self.get_nodes_to_expand(node, path)
+            for next_node in nodes_to_expand:
+                #increase depth by expanding a node
+                self.generate_routes(depth+1, next_node, path )
+        
+        #remove the current node to track back and find more paths
         path.pop()
+        #mark it as not visited, so that it can be reused to generate a different path        
         node.has_been_visited = False
 
-    def get_adjacent_nodes(self, node, parent_node, path):
-        adjacent_nodes = []
+    def get_nodes_to_expand(self, node, path):
+        nodes_to_expand = []
         #left_node
-        self.append_node(node.x-1,node.y, adjacent_nodes, parent_node, path)
+        self.add_node_to_expansion_list(node.x-1,node.y, nodes_to_expand, path)
         #right_node
-        self.append_node(node.x+1,node.y, adjacent_nodes, parent_node, path)
+        self.add_node_to_expansion_list(node.x+1,node.y, nodes_to_expand, path)
         #top_node
-        self.append_node(node.x,node.y+1, adjacent_nodes, parent_node, path)
+        self.add_node_to_expansion_list(node.x,node.y+1, nodes_to_expand, path)
         #bottom_node
-        self.append_node(node.x,node.y-1, adjacent_nodes, parent_node, path)
-        return adjacent_nodes
+        self.add_node_to_expansion_list(node.x,node.y-1, nodes_to_expand, path)
+        return nodes_to_expand
 
-    def append_node(self, x, y, adjacent_nodes, parent_node, path):
+    def add_node_to_expansion_list(self, x, y, nodes_to_expand, path):
         if x<0 or y <0 or x is self.grid_size or y is self.grid_size:
             return
         cell = self.grid[x][y]
-#         if(cell is parent_node):
-#             return
         if cell is not self.starting_node:
             if cell in path:
                 return
-        adjacent_nodes.append(cell)
-    
+        nodes_to_expand.append(cell)
 
-    def is_path_duplicate(self, path, feasible_paths):
+    def does_path_cover_same_cells(self, path, feasible_paths):
       for p in feasible_paths:
-        if(collections.Counter(p)== collections.Counter(path)):
+        if collections.Counter(p) == collections.Counter(path):
           return True
       return False
 
-
-
 if __name__ == '__main__':
-    gp = GeneratePath()
+    gp = GeneratePaths()
     gp.init_grid()
     
     gp.generate_starting_point()
     print('starting node', gp.starting_node)
-    gp.generate_route(0, gp.starting_node, None, [])
+    gp.generate_routes(0, gp.starting_node, [])
     print('Num of feasible paths', len(gp.feasible_paths))
     print("Feasible Paths ", gp.feasible_paths )
     print('length of one path',len(gp.feasible_paths[0]))
